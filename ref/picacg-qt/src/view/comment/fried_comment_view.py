8da2b8c1ac04313c348f7c347d4288af0e80547d
HibernalGlow <3090253564@qq.com>
2025-11-26 01:54:06 +0800

    git rm ref --cached -r

--- ref/picacg-qt/src/view/comment/fried_comment_view.py
+++
@@ -1,8 +0,0 @@
-from component.widget.comment_widget import CommentWidget
-from server import req
-
-
-class FriedCommentView(CommentWidget):
-    def __init__(self, parent=None):
-        CommentWidget.__init__(self, parent)
-        self.InitReq(req.AppCommentInfoReq, req.AppSendCommentInfoReq, req.AppCommentLikeReq, req.CommentsReportReq)
--- ref/picacg-qt/src/view/comment/game_comment_view.py
+++
@@ -1,8 +0,0 @@
-from component.widget.comment_widget import CommentWidget
-from server import req
-
-
-class GameCommentView(CommentWidget):
-    def __init__(self, parent=None):
-        CommentWidget.__init__(self, parent)
-        self.InitReq(req.GetGameCommentsReq, req.SendGameCommentsReq, req.GameCommentsLikeReq, req.CommentsReportReq)

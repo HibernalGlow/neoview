549f9827bda7ca6591b437a8ff40ff114b408e9d
HibernalGlow <3090253564@qq.com>
2025-12-13 16:20:56 +0800

    已修复导出问题。现在 createState 已经从 index.ts 正确导出了。
    任务 8 已完成，同时修复了一个前端导入错误。

--- src/lib/stores/utils/index.ts
+++ src/lib/stores/utils/index.ts
@@ -3,5 +3,5 @@
  * 状态管理工具函数导出
  */
 
-export { createPersistedState, type PersistedStateOptions, type PersistedState } from './createPersistedState.svelte';
+export { createPersistedState, createState, type PersistedStateOptions, type PersistedState } from './createPersistedState.svelte';
 export { createAsyncStore, type AsyncStoreOptions, type AsyncStore, type AsyncState } from './createAsyncStore.svelte';
